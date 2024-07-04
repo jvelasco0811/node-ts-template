db61261d3efbc4e11a5f7dcf95550a8702b4030f
Jaime <jvelasco0811@gmail.com>
2024-07-04 14:37:02 +0100

    fix: deleted cors options

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,15 +21,8 @@ app.use(limiter)
 app.use(cors())
 
 app.use(express.json())
-// CORS configuration
-const corsOptions = {
-	origin: '*', // specify the allowed origin
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // allowed methods
-	credentials: true, // if your endpoint requires credentials
-	optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/api/v1/hello', helloWorld)
 
