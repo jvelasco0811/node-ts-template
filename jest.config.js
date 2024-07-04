86bca91d9b9e3e7b5cdf47bc1bf35470fb7f374e
Jaime <jvelasco0811@gmail.com>
2024-07-04 17:40:14 +0100

    feat: added husky

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
 	transform: {
 		'\\.ts$': '@swc/jest',
 	},
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: -10,
+		},
+	},
 }
