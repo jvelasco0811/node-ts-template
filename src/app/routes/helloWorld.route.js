5c7f5be3aabaa93d4409373c57aa03f63d25319c
Jaime <jvelasco0811@gmail.com>
2024-07-04 16:52:44 +0100

    fix: test eslint

--- src/app/routes/helloWorld.route.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-var express_1 = require('express')
-var HelloWorldController_1 = require('../controllers/HelloWorldController')
-var router = (0, express_1.Router)()
-router.get('/', HelloWorldController_1.HelloWorldController)
-exports.default = router
