c88a1131823006b07ab78269d3518a1c97d6f407
Jaime <jvelasco0811@gmail.com>
2024-07-04 13:46:10 +0100

    feat: added helmet and ratelimit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,25 @@
 import express, { Express } from 'express'
 import helloWorld from './routes/helloWorld.route'
 import cors from 'cors'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const app: Express = express()
-app.disable('x-powered-by')
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message: 'Too many requests',
+})
+
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard({ action: 'deny' }))
+
+app.use(limiter)
+
+app.use(cors())
+
 app.use(express.json())
 // CORS configuration
 const corsOptions = {
