085f46fd745da4e3f48c630b2519ff4075f4ead1
Jaime <jvelasco0811@gmail.com>
2024-07-04 13:33:34 +0100

    fix: _req

--- src/app/controllers/HelloWorldController.ts
+++ src/app/controllers/HelloWorldController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-export const HelloWorldController = (req: Request, res: Response) => {
+export const HelloWorldController = (_req: Request, res: Response) => {
 	try {
 		res.status(200).json({ message: 'Hello World!' })
 	} catch (error: unknown) {
