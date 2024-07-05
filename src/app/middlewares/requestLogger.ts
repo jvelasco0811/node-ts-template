fa42968ea17c4d062ee45606139869ef5b30ce20
Jaime <jvelasco0811@gmail.com>
2024-07-05 17:13:50 +0100

    fix: unknownendpoint

--- src/app/middlewares/requestLogger.ts
+++ src/app/middlewares/requestLogger.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 import { logger } from '../../Context/Shared/utils/logger'
-
+import onFinished from 'on-finished'
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+	const start = Date.now()
 	const requestDetails = `${req.method} ${req.path} [${req.ip}]`
-	logger.info(requestDetails)
+
+	onFinished(res, (_err, res) => {
+		const duration = Date.now() - start
+		const contentLength = (res.getHeader('Content-Length') || 0) as string
+		const statusCode = res.statusCode
+		const logMessage = `${requestDetails} - ${statusCode} - ${contentLength} bytes - ${duration} ms`
+		logger.info(logMessage)
+	})
 
 	next()
 }
--- src/app/middlewares/unknownEndpoint.ts
+++ src/app/middlewares/unknownEndpoint.ts
@@ -1,10 +1,6 @@
 import { Request, Response } from 'express'
-import { logger } from '../../Context/Shared/utils/logger'
 
 const unknownEndpoint = (req: Request, res: Response) => {
-	const requestDetails = `Method: ${req.method}, Path: ${req.path}, ${req.statusCode}`
-	logger.info(requestDetails)
-
 	res.status(404).send({ error: 'oops not found' })
 }
 
