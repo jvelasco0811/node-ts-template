0ddd1e1bcb3d11adbf23afe888d3e3765a0dc601
Jaime <jvelasco0811@gmail.com>
2024-07-05 16:56:36 +0100

    fix: testing

--- src/app/middlewares/requestLogger.ts
+++ src/app/middlewares/requestLogger.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { logger } from '../../Context/Shared/utils/logger'
 
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-	const requestDetails = `Method: ${req.method}, Path: ${req.path}, Body: ${req.body}`
+	const requestDetails = `${req.method} ${req.path} [${req.ip}]`
 	logger.info(requestDetails)
 
 	next()
--- src/app/middlewares/unknownEndpoint.ts
+++ src/app/middlewares/unknownEndpoint.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express'
 import { logger } from '../../Context/Shared/utils/logger'
 
 const unknownEndpoint = (req: Request, res: Response) => {
-	const requestDetails = `Method: ${req.method}, Path: ${req.path}`
-	logger.info(requestDetails, 'not good')
+	const requestDetails = `Method: ${req.method}, Path: ${req.path}, ${req.statusCode}`
+	logger.info(requestDetails)
 
 	res.status(404).send({ error: 'oops not found' })
 }
